baf9f01296ad397c7f1a631bd4f5866172a113cf
Tobias Virgona <tobiasvirgona@gmail.com>
2022-10-12 19:18:48 +1100

    added helpers. Adding GET request - not working so far

--- routes/notes.js
+++ routes/notes.js
@@ -1,25 +1,29 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-// require
+const { readFromFile, readAndAppend } = require('../helper/fsUtils');
 
-// GET Route for retrieving all the tips
+
+
+// GET Route for retrieving notes
 notes.get('/', (req, res) => {
-    // get the info from notes.json and send it to the client
+    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+    console.log(data);
 });
 
-// POST Route for a new UX/UI tip
-notes.post('/', (req, res) => {
-    console.log(req.body);
-    // get the data from the request body
-    const uId = uuidv4();
-    console.log(uId)
-    // add an id property to that data obj
-    // "add that data prop to the db.json"
-    // get the data from db.json
-    // parse it
-    // add the new notes
-    // write it back to the file
-    // send the user a copy of the new note
-});
+// // POST Route for a new UX/UI tip
+// notes.post('/', (req, res) => {
+//     console.log(req.body);
+
+//     // get the data from the request body
+//     const uId = uuidv4();
+//     console.log(uId)
+//     // add an id property to that data obj
+//     // "add that data prop to the db.json"
+//     // get the data from db.json
+//     // parse it
+//     // add the new notes
+//     // write it back to the file
+//     // send the user a copy of the new note
+// });
 
 module.exports = notes;
